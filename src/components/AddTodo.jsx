320d310066b458e20cd8549193894c8fdfebd3d2
vishu pratap <vishurizz01@gmail.com>
2024-06-06 16:33:35 +0530

    more optimised by useRef hook

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,24 +1,20 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
   
 
 
 function AddTodo({ handleNewItem}) {
 
-  const [todoName, setTodoName] = useState()
-const [dueDate, setDueDate] = useState()
-
-function handleName(e){
-  setTodoName(e.target.value)
-}
-function handleDate(e){
-  setDueDate(e.target.value)
-}
-
+  const todoNameElement = useRef();
+  const todoDueDate = useRef();
+  
 function handleAddButton(){
-  handleNewItem(todoName, dueDate)
-  setDueDate("")
-  setTodoName("")
+  const valueName = todoNameElement.current.value;
+  const valueDate = todoDueDate.current.value;
+  todoNameElement.current.value = ""
+  todoDueDate.current.value = ""
+  handleNewItem(valueName, valueDate)
+  
 }
 
   return (
@@ -26,9 +22,9 @@ function handleAddButton(){
       <div className="container " id='AddTodo'>
             <div class="row cust-todo">
               <div class="col-2 border-1 mr-16 ">
-                <input value={todoName} onChange={handleName}  type="text" placeholder="Enter to-do here" /></div>
+                <input  ref={todoNameElement}  type="text" placeholder="Enter to-do here" /></div>
               <div class="col-2 border-1 ml-28 ">
-                <input value={dueDate} onChange={handleDate} type="date" placeholder="Enter to-do here"/></div>
+                <input ref={todoDueDate} type="date" placeholder="Enter to-do here"/></div>
               <div class="col-2 ml-36 "> <button onClick={handleAddButton} type="button" class="btn btn-success cust-button" id='Add'>Add</button></div>
             </div>
           </div>
